aae8de4e45867c3ee196726c572e272ce76b372b
Ams <adriansims62781@gmail.com>
2016-11-07 23:19:38 +0000

    Initial commit

---
+++ wpath/PathInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpath
+{
+    internal class PathInfo : DependencyObject
+    {
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+
+        public static readonly DependencyProperty PathProperty =
+            DependencyProperty.Register("Path", typeof(string), typeof(PathInfo), new PropertyMetadata(""));
+
+        public ObservableCollection<string> PathList { get; set; }
+
+        public PathInfo()
+        {
+            var pathvar = Environment.GetEnvironmentVariable("PATH");
+            var splitlist = pathvar?.Split(';').ToList();
+
+            PathList = new ObservableCollection<string>(splitlist);
+        }
+    }
+}
---
+++ wpath/RowNumberConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace wpath
+{
+    internal class RowNumberConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var row = value as DataGridRow;
+            if (row == null)
+            {
+                throw new InvalidOperationException("This converter class can only be used with DataGridRow elements.");
+            }
+            return row.GetIndex() + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
